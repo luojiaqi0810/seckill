57ad52f8152599ec9f2c3a231d3c9a8b2342c5cf
birdegg <313993678@qq.com>
2019-10-22 16:55:20 +0800

    add test for dao

---
+++ src/test/java/org/seckill/dao/SuccessKilledDaoTest.java
@@ -0,0 +1,36 @@
+package org.seckill.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.entity.SuccessKilled;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring配置文件在哪
+@ContextConfiguration("classpath:spring/spring-dao.xml")
+public class SuccessKilledDaoTest {
+
+    @Resource
+    private SuccessKilledDao successKilledDao;
+
+    @Test
+    public void testInsertSuccessKilled() {
+        long id = 1001L;
+        long phone = 13502181181L;
+        int insertCount = successKilledDao.insertSuccessKilled(id, phone);
+        System.out.println("insertCount: " + insertCount);
+    }
+
+    @Test
+    public void testQueryByIdWithSeckill() {
+        long id = 1001L;
+        long phone = 13502181181L;
+        SuccessKilled successKilled = successKilledDao.queryByIdWithSeckill(id, phone);
+        System.out.println(successKilled);
+        System.out.println(successKilled.getSeckill());
+
+    }
+}
