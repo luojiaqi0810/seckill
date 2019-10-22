287e6e98a58df839610af6dbd46ae8fbe23d0d97
birdegg <313993678@qq.com>
2019-10-22 16:49:04 +0800

    add SeckillDao

---
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -0,0 +1,39 @@
+package org.seckill.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.seckill.entity.Seckill;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author LuoJiaQi
+ * @Date 2019/10/22
+ * @Time 3:46
+ */
+public interface SeckillDao {
+
+    /**
+     * 减库存
+     * @param seckillId
+     * @param killTime
+     * @return 返回操作影响的行数
+     */
+    int reduceNumber(@Param("seckillId") long seckillId, @Param("killTime") Date killTime);
+
+    /**
+     * 根据id查询秒杀对象
+     * @param seckillId
+     * @return
+     */
+    Seckill queryById(long seckillId);
+
+    /**
+     * 根据偏移量查询秒杀商品列表
+     * @param offset
+     * @param limit
+     * @return
+     */
+    List<Seckill> queryAll(@Param("offset") int offset, @Param("limit") int limit);
+
+}
