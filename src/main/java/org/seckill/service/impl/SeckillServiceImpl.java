8d1786fb69aa4bb37b09127ba929ef04a4fc92ba
birdegg <313993678@qq.com>
2019-10-22 21:14:54 +0800

    add annotation @Transaction

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -15,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import java.util.Date;
@@ -73,6 +74,13 @@ public class SeckillServiceImpl implements SeckillService {
         return md5;
     }
 
+    @Transactional
+    /**
+     * 使用注解控制事务方法的优点：
+     * 1.开发团队达成一致约定，明确标注事务方法的编程风格
+     * 2.保证事务方法的执行时间尽可能短，不要穿插其他网络操作，指缓存redis，memcached，http，即RPC/HTTP请求，因为这些方法运行时间比较长，如果非要做这些操作，那就剥离导事务方法外部
+     * 3.不是所有的方法都需要事务，比如只有一条修改操作，只读操作不需要事务控制
+     */
     public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException, RepeatKillException, SeckillCloseException {
         if (md5 == null || !md5.equals(getMD5(seckillId))) {
             throw new SeckillException("seckill data rewrite");
