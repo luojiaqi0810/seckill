46aa09cd627cc61943c7300c722565a696121eed
birdegg <313993678@qq.com>
2019-10-24 18:19:52 +0800

    test RedisDao, and it works

---
+++ src/test/java/org/seckill/dao/cache/RedisDaoTest.java
@@ -0,0 +1,45 @@
+package org.seckill.dao.cache;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.dao.SeckillDao;
+import org.seckill.entity.Seckill;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * RedisDao Tester.
+ *
+ * @author <Authors name>
+ * @since <pre>ʮ�� 24, 2019</pre>
+ * @version 1.0
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/spring-dao.xml")
+public class RedisDaoTest {
+    private long id = 1001;
+
+    @Autowired
+    private RedisDao redisDao;
+
+    @Autowired
+    private SeckillDao seckillDao;
+
+
+    @Test
+    public void testSeckill() throws Exception {
+        // test get and put
+        Seckill seckill = redisDao.getSeckill(id);
+        if (seckill == null) {
+            seckill = seckillDao.queryById(id);//如果为空则从数据库取
+            if (seckill != null) {
+                String result = redisDao.putSeckill(seckill);
+                System.out.println(result);
+                seckill = redisDao.getSeckill(id);
+                System.out.println(seckill);
+            }
+        }
+    }
+} 
