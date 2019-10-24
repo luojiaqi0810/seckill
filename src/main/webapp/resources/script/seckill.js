32301a90692e991e65aec5f299f1a25ab2c0dedb
birdegg <313993678@qq.com>
2019-10-24 17:19:51 +0800

    seckill execution

--- src/main/webapp/resources/script/seckill.js
+++ src/main/webapp/resources/script/seckill.js
@@ -5,10 +5,58 @@ var seckill = {
     URL: {
         now: function () {
             return '/seckill/time/now';
+        },
+        exposer: function (seckillId) {
+            return '/seckill/' + seckillId + '/exposer';
+        },
+        execution: function (seckillId, md5) {
+            return '/seckill/' + seckillId + '/' + md5 + '/execution';
+
         }
     },
-    handleSeckillkill: function(){
-        //处理秒杀逻辑
+    handleSeckillkill: function(seckillId,node){
+        //获取秒杀地址，控制显示逻辑，执行秒杀
+        node.hide()
+            .html('<button class="btn btn-primary btn-lg" id="killBtn">开始秒杀</button>');//按钮
+        $.post(seckill.URL.exposer(seckillId),{},function (result) {
+           //在回调函数中，执行交互流程
+            if (result && result['success']) {
+                var exposer = result['data'];
+                if (exposer['exposed']) {
+                    //开启秒杀
+                    //获取秒杀地址
+                    var md5 = exposer['md5'];
+                    var killUrl = seckill.URL.execution(seckillId, md5);
+                    console.log("killUrl:" + killUrl);
+                    //绑定一次点击事件，用one只绑定一次，也就是用户点击多次也只发送一次请求
+                    $("#killBtn").one('click', function () {
+                        //执行秒杀请求
+                        //1：先禁用按钮，也就是只能点一次
+                        $(this).addClass('disabled');
+                        //2：发送秒杀请求
+                        $.post(killUrl, {}, function (result) {
+                            if (result && result['success']) {
+                                var killResult = result['data'];
+                                var state = killResult['state'];
+                                var stateInfo = killResult['stateInfo'];
+                                //3：显示秒杀结果
+                                node.html('<span class="label label-success">' + stateInfo + '</span>');
+                            }
+                        });
+                    });
+                    node.show();
+                } else {
+                    //未开启秒杀
+                    var now = exposer['now'];
+                    var start = exposer['start'];
+                    var end = exposer['end'];
+                    //重新计算计时逻辑
+                    seckill.countdown(seckillId, now, start, end);
+                }
+            } else {
+                console.log('result:' + result);//todo
+            }
+        });
     },
     //验证手机号
     validatePhone: function (phone) {
@@ -33,11 +81,12 @@ var seckill = {
                 //时间完成后回调事件
             }).on('finish.countdown', function () {
                 //获取秒杀地址，控制显示逻辑，执行秒杀
+                seckill.handleSeckillkill(seckillId,seckillBox)
 
             });
         } else {
             //秒杀开始
-            seckill.handleSeckillkill()
+            seckill.handleSeckillkill(seckillId,seckillBox)
         }
     },
     // 详情页秒杀逻辑
